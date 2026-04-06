c93d6a34a4f79f04a0a84a0a8dbdda7493e231a4
Alex-the-cat9 <alex649372@gmail.com>
2026-04-06 11:21:04 -0500

    todavia-sigo-sin-poder-dominar-for

---
+++ inicio del avansado.py
@@ -0,0 +1,17 @@
+inventario = []
+inventario.append("leguendario")
+inventario.append("bomba")
+inventario.append("armas")
+inventario.append(5)
+inventario.append(10)
+oro = 200
+inventario.append(oro)
+lista = ["objetos", "curacion"]
+inventario.append(lista)
+print("el inventario es:")
+print("-----------")
+print(inventario)
+print("-----------")
+#oye lo isiste muy bien en tu examen de .append en listas
+#aunque intentaste poner varios textos en un .append inventario.append("armas, "bomba")
+#te doy un 7/10 necesito mejorar mas 
