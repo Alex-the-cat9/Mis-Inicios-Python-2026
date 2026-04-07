40eefadf483cf28aa76c04a37b6cd9f5dccb2fbd
Alex-the-cat9 <alex649372@gmail.com>
2026-04-07 17:43:34 -0500

    Add files via upload

---
+++ aprendiendo .insert.py
@@ -0,0 +1,5 @@
+lista = ["nose que poner", "bomba"]
+lista.insert(0,"armas")
+print(lista)
+#entendi solo es .insert nada mas .insert(0, "objeto") lo importante es el primer numero aunque 
+#mi maestro me dijo que .append es ligeramente mejor porque le facilita a la computadora
