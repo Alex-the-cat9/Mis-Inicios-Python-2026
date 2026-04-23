d4a671df3b5e09eb3881bab4cf5352f24e992383
Alex-the-cat9 <alex649372@gmail.com>
2026-04-23 18:50:42 -0500

    Add files via upload

---
+++ aprendiendo in.py
@@ -0,0 +1,18 @@
+#vamos a ver si dominas el in para validar datos antes de procesarlos.
+#Misión:
+#Crea un diccionario llamado credenciales con: "usuario": "Alex" y "rol": "Grey Hat".
+#Crea una variable llamada busqueda que valga "password".
+#Usa un if con in para verificar si la palabra de busqueda está dentro de las llaves de credenciales.
+#Si está, imprime: "🔓 Accediendo a: [busqueda]".
+#Si no está, imprime: "🚨 Alerta: Intento de acceso a llave inexistente: [busqueda]".
+#credenciales = {
+#    "usuario":"Alex",
+#    "rol":"grey hat"
+#}
+#busqueda = "password"
+#for password in credenciales:
+#    if "busqueda" in password:
+#        print(f"accediendo a:{password}")
+#    else:
+#        print(f"Alerta:intento de acceso a llave inexistente:{password}")
+#Alex:easy
