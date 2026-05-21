4fc520a72d7773c9b15a9b3c3d36942bd5224ed0
Alex-the-cat9 <alex649372@gmail.com>
2026-05-21 16:07:55 -0500

    Add files via upload

---
+++ POO/DECORADORES/decoradores.py
@@ -0,0 +1,37 @@
+# 🛡️ 1. LA CAJA EXTERNA: El decorador que recibe el plano de la función
+def firewall_militar(funcion_original):
+    
+    # 📦 2. LA CAJA INTERNA: El traje que mete los superpoderes en los chips
+    def envoltura():
+        print("\n🔒 [FIREWALL] Escaneando memoria caché en busca de malware...")
+        print("🕵️‍♂️ [FIREWALL] Verificando integridad de la terminal de Windows...")
+        
+        # 🚀 Aquí se dispara físicamente el cable de la acción original
+        funcion_original()
+        
+        print("✅ [FIREWALL] Operación terminada de forma segura. Circuito cerrado.")
+        
+    # 🔌 3. REGRESAMOS EL PAQUETE: Retornamos la envoltura armada (Sin paréntesis)
+
+
+# --- INYECTANDO LOS ESCUDOS EN LAS FUNCIONES ---
+
+@firewall_militar  # 🚀 Inyectamos el blindaje sin tocar el código de abajo
+def arrancar_servidor():
+    print("💻 [SISTEMA] Inicializando los núcleos de la Inteligencia Artificial...")
+
+# 🚀 Reutilizamos el mismo traje para un comando totalmente distinto
+@firewall_militar
+def abrir_caja_fuerte():
+    print("🏦 [BOVEDA] Abriendo compuertas magnéticas de la caja registradora...")
+
+
+
+print("🛰️  === INICIALIZANDO CONSOLA DE CONFIGURACIÓN ===")
+    
+# Disparamos las funciones normales, pero verás el Firewall activarse en tu pantalla
+abrir_caja_fuerte()
+abrir_caja_fuerte()
+#Alex:el de abajo que dice funcion original y luego haces @firewal_militar y abajo una funcion esa funcion de abajo
+#ya esta cumpliendo esta dentro de la funcion original de firewal militar?
+#maestro.IA:¡SÍ, EXACTO, ALEX! DISTE EN EL CLAVO ABSOLUTO DE LA MAGIA NEGRA DE PYTHON. 🎯💥 El procesador hace exactamente eso por debajo
