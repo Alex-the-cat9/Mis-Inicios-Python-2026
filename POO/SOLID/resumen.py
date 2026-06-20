42ac3113b1c962c7f3c8c92331738d7b11a873d3
Alex-the-cat9 <alex649372@gmail.com>
2026-06-20 17:48:33 -0500

    Add files via upload

---
+++ POO/SOLID/resumen.py
@@ -0,0 +1,11 @@
+#S:es solo seguir el nombre si una clase o funcion dice que mueve el carro pues lo unico que deve hacer es mover el carro nada mas
+# quizas gastar gasolina puede ser pero ay tambien puede hacerlo otra funcion o clase
+#O:yo lo aprendi con clases abstractas que la clase abstracta no se toca pero puedes modificar a sus clases hijas pero al padre no
+# porque si modificas a la clase abstracta todo sale codigo rojo es como hacer codigo dentro del codigo
+#L:es solo seguir las reglas que ponen eh puesto mypy para que sea mas estricto y seguir siempre estas reglas
+# las reglas lo ponen como -> si la clase dice que va devolver algo lo deve de cumplir si o si porque el error no dara si no lo cumples
+# sino lo dara el que lo resive que no tiene datos para hacer su trabajo
+#I:no meter metodos inecesarios eso es todo no meter metodos inecesarios metodos que no va usar
+#D:en vez de hacer self.combustible = 500 puedes hacer una clase combustible que tenga ese self.combustible asi haces
+#self.combustible.tanque es como un tanque que da combustible a muchas clases en vez de estar poniendo numeros
+#o heredar de esa clase es mas facil asi
